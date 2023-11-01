d381de52b43a0f9a18c64600d1fb28adfd3105d3
relt <haxrelt@gmail.com>
2023-11-01 12:22:29 +0300

    Directory Service

--- vfs/directory_service.cpp
+++ vfs/directory_service.cpp
@@ -1,1 +1,11 @@
 #include "directory_service.h"
+
+DirectoryService::DirectoryService()
+{
+	_file_system = nullptr;
+}
+
+DirectoryService::DirectoryService(FileSystem* file_system)
+{
+	_file_system = file_system;
+}
--- vfs/directory_service.h
+++ vfs/directory_service.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "file_system.h"
 class DirectoryService
 {
+private:
+	FileSystem* _file_system;
+
+public:
+	DirectoryService();
+	DirectoryService(FileSystem* file_system);
+
+	Directory Create(std::string name);
+	Directory Update();
+	void Delete();
 };
 
