a92d7ff6b8a7bc0f44e6fa5e95a7a5aa347e0ccd
relt <haxrelt@gmail.com>
2023-11-01 12:04:19 +0300

    Block repository initial

--- vfs/block_repository.cpp
+++ vfs/block_repository.cpp
@@ -1,1 +1,15 @@
 #include "block_repository.h"
+
+BlockRepository::BlockRepository() : Repository()
+{
+	this->_fat = nullptr;
+	this->_fat_location = 0;
+	this->_record_size = 0;
+}
+
+BlockRepository::BlockRepository(std::string name, Superblock* sb) : Repository(name)
+{
+	_fat = new FAT(sb->fat_capacity());
+	_fat_location = sb->num_of_first_fat_block() * sb->block_size();
+	_record_size = sizeof(_fat[0]);
+}
--- vfs/block_repository.h
+++ vfs/block_repository.h
@@ -1,6 +1,23 @@
 #pragma once
 #include "repository.h"
+#include "fat.h"
+#include "superblock.h"
+#include "block.h"
+
 class BlockRepository : public Repository
 {
+private:
+	FAT* _fat;
+	uint_fast16_t _record_size;
+	uint_fast32_t _fat_location;
+
+public:
+	BlockRepository();
+	BlockRepository(std::string name, Superblock* sb);
+
+	void Save(Block* block);
+	Block* GetById(int id);
+	Block* FindFree();
+	Block* Allocate();
 };
 
