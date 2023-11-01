a89e026059832e711425f10d997361e41f3e9f6e
relt <haxrelt@gmail.com>
2023-11-01 12:20:38 +0300

    File Service

--- vfs/file_service.cpp
+++ vfs/file_service.cpp
@@ -1,1 +1,11 @@
 #include "file_service.h"
+
+FileService::FileService()
+{
+	_file_system = nullptr;
+}
+
+FileService::FileService(FileSystem* file_system)
+{
+	_file_system = file_system;
+}
--- vfs/file_service.h
+++ vfs/file_service.h
@@ -1,5 +1,17 @@
 #pragma once
+#include "file_system.h"
+
 class FileService
 {
+private:
+	FileSystem* _file_system;
+
+public:
+	FileService();
+	FileService(FileSystem* file_system);
+
+	File Create(std::string name);
+	File Update();
+	void Delete();
 };
 
