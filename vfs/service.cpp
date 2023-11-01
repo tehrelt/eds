f3e56518962444f17469f93960db2eb9c536ae69
relt <haxrelt@gmail.com>
2023-11-01 12:25:05 +0300

    Service

--- vfs/service.cpp
+++ vfs/service.cpp
@@ -1,1 +1,13 @@
 #include "service.h"
+
+Service::Service()
+{
+	_file_service = nullptr;
+	_directory_service = nullptr;
+}
+
+Service::Service(FileSystem* file_system)
+{
+	_file_service = FileService(file_system);
+	_directory_service = DirectoryService(file_system);
+}
--- vfs/service.h
+++ vfs/service.h
@@ -1,5 +1,18 @@
 #pragma once
+#include "file_service.h"
+#include "directory_service.h"
+
 class Service
 {
+private:
+	FileService _file_service;
+	DirectoryService _directory_service;
+
+public:
+	Service();
+	Service(FileSystem* file_system);
+
+	FileService file_service()				{ return _file_service; }
+	DirectoryService directory_service()	{ return _directory_service; }
 };
 
